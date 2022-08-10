4455afaa5e03dc380a779d11edfd23ab3a1312a9
Murat Deniz <murat.deniz@teamdefinex.com>
2022-08-10 12:12:42 +0300

    catch error for unhandle errors

--- src/index.js
+++ src/index.js
@@ -6,19 +6,24 @@ import ITO from './lib/ito.js';
 const ito = new ITO();
 
 const init = async () => {
-  const { file, out, proxy } = commandParser();
+  try {
+    const { file, out, proxy } = commandParser();
 
-  if ( proxy ) global.$proxyList = JSON.parse( await fsPromises.readFile( proxy, 'UTF-8' ) );
-  const sicNumbers = JSON.parse( await fsPromises.readFile( file, 'UTF-8' ) );
+    if ( proxy ) global.$proxyList = JSON.parse( await fsPromises.readFile( proxy, 'UTF-8' ) );
+    const sicNumbers = JSON.parse( await fsPromises.readFile( file, 'UTF-8' ) );
 
-  /* Concurrency max 5 task! */
-  const companyList = await parallelLimit( sicNumbers.map( n => ito.getCompanyWithSicNumber.bind( ito, n ) ), 5 );
-  const filteredCompanyList = companyList.filter( c => !!c );
+    /* Concurrency max 5 task! */
+    const companyList = await parallelLimit( sicNumbers.map( n => ito.getCompanyWithSicNumber.bind( ito, n ) ), 5 );
+    const filteredCompanyList = companyList.filter( c => !!c );
 
-  const outPath = out || `./out-${Date.now()}.json`;
-  await fsPromises.writeFile( outPath, JSON.stringify( filteredCompanyList, null, 2, 2 ), 'UTF-8' );
+    const outPath = out || `./out-${Date.now()}.json`;
+    await fsPromises.writeFile( outPath, JSON.stringify( filteredCompanyList, null, 2, 2 ), 'UTF-8' );
 
-  console.info( `Script finished successfully, you can the find your out file at ${outPath}` );
+    console.info( `Script finished successfully, you can the find your out file at ${outPath}` );
+  } catch ( e ) {
+    console.error( e );
+    process.exit( 1 );
+  }
 };
 
 init();
